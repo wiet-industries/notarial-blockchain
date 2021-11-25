8aff42102cfb9d6b5c73a3f90fd2d331f576760e
xsanm <kkurowski.kk@gmail.com>
2021-11-25 15:37:06 +0100

    WIP Jenkins setup v1

--- src/main/core/Server.java
+++ src/main/core/Server.java
@@ -2,7 +2,7 @@ import static java.lang.Thread.sleep;
 
 public class Server {
     public static void main(String[] args) throws InterruptedException {
-       for(int i = 0; i < 1000; i++) {
+        for(int i = 0; i < 1000; i++) {
             System.out.println("server working.. [" + i + "]");
             sleep(3000);
         }
