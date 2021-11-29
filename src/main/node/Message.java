b94cbbd67bb0c392a065ac478f4727d9083923a7
Atheam <adam.dyda1@gmail.com>
2021-11-30 00:28:35 +0100

    delete sout

--- src/main/node/Message.java
+++ src/main/node/Message.java
@@ -33,7 +33,6 @@ public class Message {
     Message fromBytes(byte[] data) {
         System.out.println(new String(data));
         String[] split = new String(data).split(MESSAGE_TYPE_SEPARATOR);
-        System.out.println("Bug: "+split[0]);
         type = MessageType.valueOf(split[0]);
         ID = Integer.parseInt(split[1]);
         if(split.length == 3) content = split[2];
