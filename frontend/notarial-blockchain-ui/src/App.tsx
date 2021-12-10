5e997bf92fdaa2269507da63ae15e5dc7f6b6841
adrianryt <adrianryt3@gmail.com>
2021-12-10 17:50:01 +0100

    add axios usability to UI

--- frontend/notarial-blockchain-ui/src/App.tsx
+++ frontend/notarial-blockchain-ui/src/App.tsx
@@ -1,28 +1,93 @@
 import './App.css';
 import Button from '@mui/material/Button';
+import axios from "axios";
+import {useState} from "react";
+import {CircularProgress} from "@material-ui/core";
+
+type ConnectionType = "NOT_CONNECTED" | "CONNECTING_ERROR" | "CONNECTED";
+type User = {
+  port?: string;
+  ip?: string;
+}
 
 const  App = () => {
+  const [connected, setConnected] = useState<ConnectionType>("NOT_CONNECTED");
+  const [loading, setLoading] = useState(false);
+  const [user, setUserData] = useState<User>({});
+  const [message, setMessage] = useState<String[]>([]);
+
+
+  const connect = () => {
+    setLoading(true);
+    setMessage(prevState => ["Connecting", ...prevState.slice(0,6)]);
+    axios.post('http://localhost:8080/node/connect')
+        .then(response => {
+          if(response.status === 200 && response.data.message === "OK") {
+            setMessage(prevState => ["Connected successfully", ...prevState.slice(0,6)]);
+            setConnected("CONNECTED");
+            setUserData({port: response.data.port, ip: response.data.ip})
+          } else {
+            setMessage(prevState => ["Error while connecting", ...prevState.slice(0,6)]);
+            setConnected("CONNECTING_ERROR");
+          }})
+        .catch(() => {
+            setMessage(prevState => ["Error while connecting", ...prevState.slice(0,6)]);
+            setConnected("CONNECTING_ERROR");
+        })
+        .finally(() => setLoading(false));
+  }
+
+  const broadcast = () => {
+    axios.post('http://localhost:8080/node/broadcast')
+        .then(response => {
+          if(response.status === 200 && response.data.message === "OK") {
+              setMessage(prevState => [ "Broadcasting", ...prevState.slice(0,6)]);
+          } else {
+              setMessage(prevState => ["Error while broadcasting", ...prevState.slice(0,6)])
+          }
+        }).catch(() => setMessage(prevState => ["Error while broadcasting", ...prevState.slice(0,6)]))
+        .finally(() => setMessage(prevState => ["End of broadcasting", ...prevState.slice(0,6)]));
+  }
+  const disconnect = () => {
+    setMessage(prevState => ["Disconnecting", ...prevState.slice(0,6)]);
+    axios.delete('http://localhost:8080/node/disconnect')
+        .then(response => {
+          if(response.status === 200 && response.data.message === "OK") {
+              setMessage(prevState => ["Disconnected from server", ...prevState.slice(0,6)]);
+          }
+        }).catch(() => setMessage(prevState => ["Error while disconnecting", ...prevState.slice(0,6)]))
+  }
 
   return (
     <div className="App">
       <header className="App-header">
+        {!loading && (<p>{connected === "NOT_CONNECTED" ? "Not connected!" : "CONNECTING_ERROR" ? "Error while connecting" : "Connected"}</p>)}
+        {loading && (<CircularProgress />)}
         <p>
           notarial-blockchain
         </p>
+        <div className="my-5">
+          <p>Port: {user.port}</p>
+          <p>IP: {user.ip}</p>
+        </div>
+
         <div className="d-flex justify-content-around w-50">
-          <Button variant="contained">Connect to server</Button>
-          <Button variant="contained">Send broadcast to server</Button>
-          <Button variant="contained">Disconnect from server</Button>
+          <Button variant="contained" onClick={connect}>Connect to server</Button>
+          <Button variant="contained" onClick={broadcast}>Send broadcast to server</Button>
+          <Button variant="contained" onClick={disconnect}>Disconnect from server</Button>
         </div>
 
         <div className="my-5">
-          <p>TU JEST MESSAGE</p>
+            <ul>
+                {message.map(item => {
+                    return <li>{item}</li>;
+                })}
+            </ul>
         </div>
 
       </header>
     </div>
   );
 }
-
 
 export default App;
