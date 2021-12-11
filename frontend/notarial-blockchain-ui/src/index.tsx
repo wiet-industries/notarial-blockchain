3e000f2427399f5123d26ce292485eb4b7c86f40
xsanm <kkurowski.kk@gmail.com>
2021-12-11 15:09:34 +0100

    Add prettier to ESLint

--- frontend/notarial-blockchain-ui/src/index.tsx
+++ frontend/notarial-blockchain-ui/src/index.tsx
@@ -1,18 +1,18 @@
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.css';
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.css";
 
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 // eslint-disable-next-line import/no-unresolved
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root'),
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
